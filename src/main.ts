a8a60a96a0c605946dbcc7a258fd26edfc05bc18
Osouzaa <gabrielsouzaalves750@gmail.com>
2025-04-28 09:58:18 -0300

    feat; add config

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,23 @@ import { env } from './env';
 import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const keyFile = fs.readFileSync(__dirname + '/../src/certs/key.pem');
+  const certFile = fs.readFileSync(__dirname + '/../src/certs/cert.pem');
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile
+    }
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors({ origin: '*' });
 
   app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "frame-ancestors *");
+    res.setHeader('Content-Security-Policy', 'frame-ancestors *');
     next();
   });
 
